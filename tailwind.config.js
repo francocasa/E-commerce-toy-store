095d9dad1334620c34210e150ea49df9fefb7372
Mikelyto1994 <mikelyto@gmail.com>
2024-09-25 15:27:18 -0500

    Actualizar segun main/develop

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,6 +1,6 @@
 /** @type {import('tailwindcss').Config} */
-module.exports = {
-  content: ['./index.html', './src/**/*.{js,jsx,ts,tsx}'],
+export default {
+  content: ['./index.html', './src/**/*.{js,ts,jsx,tsx}'],
   theme: {
     extend: {},
   },
