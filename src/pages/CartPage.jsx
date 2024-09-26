549ff0527db430fdcda7a737cb0122d7c5ae475d
Bubbl33s <jairo.lf.8@gmail.com>
2024-09-26 11:54:29 -0500

    feat: add cart item with static data

--- src/pages/CartPage.jsx
+++ src/pages/CartPage.jsx
@@ -1,12 +1,17 @@
+import { CartItem } from '../components';
+
 function CartPage() {
   return (
     <main className="container mx-auto p-8">
-      <div className="flex">
-        <section className="border rounded-lg shadow w-3/4">
-          <h1>Carrito</h1>
-          <article className="">
-            <p>Product Item Component</p>
-          </article>
+      <div className="flex gap-3 mt-7">
+        <section className="border rounded-lg shadow w-3/4 p-6">
+          <h1 className="font-medium mb-4 text-xl">Carrito</h1>
+
+          <div className="">
+            <CartItem />
+            <CartItem />
+            <CartItem />
+          </div>
         </section>
         <aside className="border rounded-lg shadow w-1/4">
           <p>Resumen de compra component</p>
