e149d110aaa20b0e2cc5fa3ff84fec907343506e
Bubbl33s <jairo.lf.8@gmail.com>
2024-10-02 10:55:19 -0500

    feat: add footer mobile responsive

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -3,49 +3,63 @@ import { FaWhatsapp } from 'react-icons/fa';
 
 export default function Footer() {
   return (
-    <footer className="bg-blue-50 py-12 text-gray-800 relative">
-      <div className="w-full sm:w-3/4 mx-auto flex flex-col sm:flex-row justify-between gap-6 p-4">
+    <footer className="bg-blue-50 py-6 px-4 lg:py-12 text-gray-800 relative">
+      <div className="container mx-auto flex flex-col sm:flex-row justify-between gap-6 lg:py-4 lg:p-4">
         {' '}
         {/* Agregado p-4 para márgenes */}
-        <div className="flex w-full sm:w-1/2">
-          <div className="w-1/2">
-            <img src="/logo-juguetitos.png" alt="Toys Logo" className="w-3/5" />
+        <div className="flex w-full sm:w-1/2 gap-3">
+          <div className="px-3 w-1/2">
+            <img
+              src="/logo-juguetitos.png"
+              alt="Toys Logo"
+              className="md:w-40"
+            />
           </div>
-          <ul className="list-none space-y-4">
+          <ul className="list-none space-y-1 md:space-y-4">
             <li>
-              <Link to={`/aboutus`}>
-                <a href="#">Quiénes somos</a>
+              <Link
+                to={`/aboutus`}
+                className="cursor-pointer hover:text-gray-500 text-base"
+              >
+                Quiénes somos
               </Link>
             </li>
             <li>
-              <Link to={`/politica`}>
-                <a href="#">Política de privacidad</a>
+              <Link
+                to={`/politica`}
+                className="cursor-pointer hover:text-gray-500 text-base"
+              >
+                Política de privacidad
               </Link>
             </li>
             <li>
-              <Link to={`/faq`}>
-                <a href="#">Preguntas y respuestas</a>
+              <Link
+                to={`/faq`}
+                className="cursor-pointer hover:text-gray-500 text-base"
+              >
+                Preguntas y respuestas
               </Link>
             </li>
           </ul>
         </div>
-        <div className="w-full sm:w-1/2">
+        <hr className="border-y-[1px] mx-3 block md:hidden" />
+        <div className="px-3 w-full md:w-1/2">
           <p className="font-bold mb-1">Hable con nosotros</p>
           <form>
             <input
               type="text"
               placeholder="Nombre"
-              className="border p-2 w-full mb-4"
+              className="border p-1 w-full mb-2 text-sm"
             />
-            <input
-              type="text"
-              placeholder="Escribe tu mensaje"
-              className="border p-2 w-full mb-4"
-            />
+            <textarea
+              rows={3}
+              placeholder="Escribe tu mensaje..."
+              className="border p-1 w-full mb-2 text-sm"
+            ></textarea>
             <input
               type="submit"
               value="Enviar mensaje"
-              className="bg-blue-500 text-white py-3 px-5 mt-2"
+              className="bg-blue-500 text-white py-2 px-3 text-sm"
             />
           </form>
         </div>
