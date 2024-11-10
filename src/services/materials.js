d4378c5dc2fae92b7eba3847b407aee25e03470e
Miguel Pariona <157014665+Mikelyto1994@users.noreply.github.com>
2024-11-09 19:19:40 -0500

    Update materials.js

--- src/services/materials.js
+++ src/services/materials.js
@@ -2,14 +2,71 @@ import axios from 'axios';
 
 const BASE_URL = import.meta.env.VITE_API_URL; // Obtener la URL base desde el .env
 
+// Función para consultar los materiales inhabilitados
+export const consultaMaterialesInhabilitados = async () => {
+  const URL = `${BASE_URL}/materials`;
+  try {
+    const response = await axios.get(URL);
+    // Filtrar solo los materiales que están inhabilitados (isDeleted: true)
+    return response.data.filter((material) => material.isDeleted);
+  } catch (error) {
+    console.error('Error fetching disabled materials:', error);
+    return []; // Retornar un arreglo vacío en caso de error
+  }
+};
+
+// Función para habilitar un material
+export const habilitarMaterial = async (id) => {
+  const URL = `${BASE_URL}/materials/${id}/activate`; // Endpoint para activar el material
+  const token = localStorage.getItem('adminToken'); // Obtener el token de autenticación
+
+  try {
+    const response = await axios.patch(
+      URL,
+      { isDeleted: false }, // Cambiar el valor de isDeleted a false
+      {
+        headers: {
+          Authorization: `Bearer ${token}`,
+        },
+      },
+    );
+    return response.data; // Retornar el material habilitado
+  } catch (error) {
+    console.error('Error enabling material:', error);
+    return null;
+  }
+};
+
+// Función para deshabilitar un material
+export const deshabilitarMaterial = async (id) => {
+  const URL = `${BASE_URL}/materials/${id}/deactivate`; // Endpoint para desactivar el material
+  const token = localStorage.getItem('adminToken'); // Obtener el token
+
+  try {
+    const response = await axios.patch(
+      URL,
+      { isDeleted: true }, // Cambiar el valor de isDeleted a true
+      {
+        headers: {
+          Authorization: `Bearer ${token}`,
+        },
+      },
+    );
+    return response.data; // Retornar el material deshabilitado
+  } catch (error) {
+    console.error('Error disabling material:', error);
+    return null;
+  }
+};
+
 // Función para consultar todos los materiales
 export const consultaMateriales = async () => {
   const URL = `${BASE_URL}/materials`;
   try {
     const response = await axios.get(URL);
-    return response.data; // Retornar los datos obtenidos
+    return response.data.filter((discount) => !discount.isDeleted); // Filtrar solo los habilitados
   } catch (error) {
-    console.error('Error fetching materials:', error);
+    console.error('Error fetching enabled discounts:', error);
     return []; // Retornar un arreglo vacío en caso de error
   }
 };
