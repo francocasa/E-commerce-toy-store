daedacee459399b3c3e253ef58bc03de13646c46
Bubbl33s <jairo.lf.8@gmail.com>
2024-11-19 12:01:30 -0500

    feat: hero section

--- src/components/HeroSection.jsx
+++ src/components/HeroSection.jsx
@@ -41,38 +41,39 @@ const HeroSection = () => {
 
   return (
     <section
-      className="relative h-96 bg-cover bg-center flex flex-col justify-end items-start text-white p-4 sm:p-6 lg:p-8"
+      className="relative h-52 bg-cover bg-center flex flex-col text-gray-800 justify-between items-start p-4 sm:p-6 lg:p-8"
       style={{
         backgroundImage: `linear-gradient(to bottom, rgba(0, 0, 0, 0), rgba(0, 0, 0, 0.5)), url(${heroData[currentSlide].backgroundImage})`,
       }}
     >
       <div className="text-left ml-0 sm:ml-5 mb-5">
-        <h1 className="text-2xl sm:text-4xl font-bold">
+        <h1 className="text-xl sm:text-4xl font-bold">
           {heroData[currentSlide].title}
         </h1>
-        <p className="text-sm sm:text-lg mt-4">
+        <p className="text-sm sm:text-lg mt-2">
           {heroData[currentSlide].description}
         </p>
-        <a
-          href={heroData[currentSlide].buttonLink}
-          className="mt-6 bg-blue-600 py-2 px-4 sm:px-6 rounded-md text-white hover:bg-blue-700 transition-all inline-block"
-        >
-          {heroData[currentSlide].buttonText}
-        </a>
       </div>
 
+      <a
+        href={heroData[currentSlide].buttonLink}
+        className="text-sm mb-3 text-white bg-blue-600 py-2 px-4 sm:px-6 rounded-md hover:bg-blue-700 transition-all inline-block"
+      >
+        {heroData[currentSlide].buttonText}
+      </a>
+
       {/* Controles para el slider */}
-      <div className="absolute left-0 top-1/2 transform -translate-y-1/2">
+      <div className="absolute left-2 top-1/2 transform -translate-y-1/2">
         <button
-          className="text-3xl text-white bg-black bg-opacity-50 p-2 rounded-full"
+          className="text-xl text-black bg-slate-100 bg-opacity-50 font-bold p-2 rounded-full"
           onClick={handlePrev}
         >
           {'<'}
         </button>
       </div>
-      <div className="absolute right-0 top-1/2 transform -translate-y-1/2">
+      <div className="absolute right-2 top-1/2 transform -translate-y-1/2">
         <button
-          className="text-3xl text-white bg-black bg-opacity-50 p-2 rounded-full"
+          className="text-xl text-black bg-slate-100 bg-opacity-50 font-bold p-2 rounded-full"
           onClick={handleNext}
         >
           {'>'}
@@ -80,11 +81,11 @@ const HeroSection = () => {
       </div>
 
       {/* Indicadores del slider */}
-      <div className="absolute bottom-4 left-1/2 transform -translate-x-1/2 flex space-x-2">
+      <div className="absolute bottom-2 left-1/2 transform -translate-x-1/2 flex space-x-2">
         {heroData.map((_, index) => (
           <span
             key={index}
-            className={`w-3 h-3 rounded-full ${currentSlide === index ? 'bg-white' : 'bg-gray-400'}`}
+            className={`w-2 h-2 rounded-full ${currentSlide === index ? 'bg-white' : 'bg-gray-400'}`}
           ></span>
         ))}
       </div>
