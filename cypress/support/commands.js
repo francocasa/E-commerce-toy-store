24080383aa6d6048ed23d55bd17cdb7da943e61c
Miguel Pariona <157014665+Mikelyto1994@users.noreply.github.com>
2024-11-26 16:48:06 -0500

    Create commands.js

---
+++ cypress/support/commands.js
@@ -0,0 +1,28 @@
+// cypress/support/commands.js
+Cypress.Commands.add("loginAdmin", (username = "bubbl33s", password = "Admin123_") => {
+    cy.intercept('POST', `${Cypress.env('VITE_API_URL')}/admins/login`, {
+      statusCode: 200,
+      body: {
+        admin: { username, name: 'Admin User' },
+        token: 'mocked-token', // Este es el token simulado
+      },
+    }).as('loginRequest');
+  
+    // Simular el flujo de inicio de sesión
+    cy.visit('/loginAdm');
+    cy.get('input[placeholder="Nombre de Usuario"]').type(username);
+    cy.get('input[placeholder="Contraseña"]').type(password);
+    cy.get('button[type="submit"]').click();
+  
+    // Esperar la solicitud interceptada y guardar el token en localStorage
+    cy.wait('@loginRequest').then(({ response }) => {
+      const token = response.body.token;
+      cy.window().then((window) => {
+        window.localStorage.setItem('adminToken', token); // Guardar el token
+      });
+    });
+  
+    // Confirmar el modal de éxito
+    cy.get('.swal2-confirm').click();
+  });
+  
